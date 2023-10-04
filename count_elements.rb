40a848fb8a5e4542225fc37064261f170cea2228
Karolina-Sas <karolina.sas@yahoo.com>
2023-10-04 22:29:55 +0200

    uploading files

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,10 @@
 class CountElements
-
+    def initialize(arr)
+        @arr=arr   
+        def perform
+            puts @arr.sort.tally     
+        end         
+    end  
 end
+CountElements.new([0,4,2,1,0,2,5]).perform
+
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,18 @@
 class ValidatePassword
-
+REGEX_PATTERN = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%&*+=:;?<>]).{6,24}$/ 
+# REGEX_PATTERN = /^(?=.*?[A-Z]{2})(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%&*+=:;?<>]).{6,24}$/ 
+def initialize pass
+    @pass=pass
+    def perform
+            
+    if @pass =~ REGEX_PATTERN
+        puts "true"        
+    else
+        puts "false" 
+    end
+    end
 end
+end
+
+ValidatePassword.new("Password1!").perform
+
